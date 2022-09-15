b3d9640a521745c258cf748cf8a16be5d58d38fd
Raphael Sette <raphaelsette@pm.me>
2022-09-15 14:52:50 -0300

    Adicionado sweetalert para os campos vazios

--- script.js
+++ script.js
@@ -4,16 +4,25 @@ function calculoMedia() {
     var notaTerceiroBimestre = parseInt(document.getElementById("nota-terceiro-bimestre").value);
     var notaQuartoBimestre = parseInt(document.getElementById("nota-quarto-bimestre").value);
 
-    var mediaFinal = (notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre) / 4;
-
-    var calculoMedia = document.getElementById("calculo-media");
-    calculoMedia.innerHTML = "A sua média final é " + mediaFinal.toFixed(1);
-
-    if (mediaFinal >= 6) {
-        var statusMedia = document.getElementById("status-da-media");
-        statusMedia.innerHTML = "Você está <p id='status-aprovado'>APROVADO!</p>";
+    if(!notaPrimeiroBimestre) {
+        Swal.fire({icon: 'error',title: 'Oops...',text: 'Você esqueceu de colocar a nota do Primeiro Bimestre!'});
+    } else if (!notaSegundoBimestre) {
+        Swal.fire({icon: 'error',title: 'Oops...',text: 'Você esqueceu de colocar a nota do Segundo Bimestre!'});
+    } else if (!notaTerceiroBimestre) {
+        Swal.fire({icon: 'error',title: 'Oops...',text: 'Você esqueceu de colocar a nota do Terceiro Bimestre!'});
+    } else if (!notaQuartoBimestre) {
+        Swal.fire({icon: 'error',title: 'Oops...',text: 'Você esqueceu de colocar a nota do Quarto Bimestre!'});
     } else {
-        var statusMedia = document.getElementById("status-da-media");
-        statusMedia.innerHTML = "Você está <p id='status-reprovado'>REPROVADO!</p>";
+        var mediaFinal = (notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre) / 4;
+        var calculoMedia = document.getElementById("calculo-media");
+        calculoMedia.innerHTML = "A sua média final é " + mediaFinal.toFixed(1);
+
+        if (mediaFinal >= 6) {
+            var statusMedia = document.getElementById("status-da-media");
+            statusMedia.innerHTML = "Você está <p id='status-aprovado'>APROVADO!</p>";
+        } else {
+            var statusMedia = document.getElementById("status-da-media");
+            statusMedia.innerHTML = "Você está <p id='status-reprovado'>REPROVADO!</p>";
+        }
     }
 }
